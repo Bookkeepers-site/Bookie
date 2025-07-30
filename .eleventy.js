27ab528baef5dfd877887d48c8eddccb168ccafd
Bookkeepers-site <heidi.bailey01@gmail.com>
2025-07-30 12:45:36 +0800

    Add files via upload

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("admin");
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+};
